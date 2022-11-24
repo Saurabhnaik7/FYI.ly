94af7e31ca76d0ae4a881dc5ac164a644871592b
Saurabh Naik <saurabh111799@gmail.com>
2022-11-24 19:39:12 +0530

    build for production

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const path = require('path');
 
 // Connect to MongoDB
 connectDB();
@@ -25,6 +26,16 @@ app.use(Express.json());
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/', require('./routes/createURL'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build' ,'index.html'));
+  });
+}
 
 // Specify ports for webserver
 const PORT = process.env.PORT || 5000;
