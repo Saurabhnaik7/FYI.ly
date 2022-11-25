744964dcf0f3ffc5b48a676716b922645923abac
Saurabh Naik <saurabh111799@gmail.com>
2022-11-25 11:11:28 +0530

    working on deployment

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use('/', require('./routes/createURL'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('frontend/build'));
+  app.use(Express.static('frontend/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build' ,'index.html'));
